072023f76e5bc11c036c31877baf234f22dc9f0e
SawABirdOnce <careed.1110@gmail.com>
2024-12-03 16:49:38 -0600

    lab 9 working?

--- Problem 2/driving.py
+++ Problem 2/driving.py
@@ -1,21 +1,17 @@
 
 def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
-   miles_per_gallon= input('Enter miles per gallon: ')
-   dollars_per_gallon= input('Enter dollars per gallon: ')
-   miles_driven= input('Enter miles driven: ')
-   
-   cost = (miles_driven/miles_per_gallon)*dollars_per_gallon
-   
-   return cost
+    return (miles_driven / miles_per_gallon) * dollars_per_gallon
+
 
 if __name__ == '__main__':
-    miles_per_gallon = float(input('Enter miles per gallon: '))
+    
+    miles_per_gallon = float(input("Enter miles per gallon: "))
     dollars_per_gallon = float(input("Enter dollars per gallon: "))
 
-print(f'10 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 100):.2f}')
-print(f'50 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 50):.2f}')
-print(f'400 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 400):.2f}')
-   
+    print(f'Cost for 10 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 10):.2f}')
+    print(f'Cost for 50 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 50):.2f}')
+    print(f'Cost for 400 miles: {driving_cost(miles_per_gallon, dollars_per_gallon, 400):.2f}')
+
 
    
     
